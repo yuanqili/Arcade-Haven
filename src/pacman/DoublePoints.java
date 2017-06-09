f69dce866c3fd1c0fbc0b0d9fac645b8efb3aa90
Nawakhtha <ynawakda@gmail.com>
2017-06-08 19:44:22 -0700

    Added more comments.

--- src/pacman/DoublePoints.java
+++ src/pacman/DoublePoints.java
@@ -3,13 +3,25 @@ package pacman;
 
 public class DoublePoints extends Item {
 
+    /**
+     * Activates the item by setting its point worth and duration. It then doubles the amount of points gained by doubling the variable associated with that in the grid file
+     * @param grid Is used to access the GridReadCreate variable that decided the worth of each dot.
+     * @param pac Not used in this function, but it is a parameter by the Item class that DoublePoints extends.
+     * @param timeLimit Not used in this function, but it is a parameter required by the Item class that DoublePoints extends.
+     */
     @Override
     void activate(GridReadCreate grid, Pacman pac, int[] timeLimit) {
         pointWorth = 50;
         duration = 10000;
+        grid.score += pointWorth;
         grid.pointWorth = grid.pointWorth *2;
     }
 
+    /**
+     * Deactivates the item by restoring any values it has originally modified.
+     * @param grid Is used to access the GridReadCreatevariable that decided the worth of each dot.
+     * @param pacman Not used in this function, but it is a parameter required by the Item class that DoublePoints extends.
+     */
     @Override
     public void deactivate(GridReadCreate grid, Pacman pacman) {
         grid.pointWorth = grid.pointWorth / 2;
--- src/pacman/SpeedUp.java
+++ src/pacman/SpeedUp.java
@@ -3,8 +3,17 @@ package pacman;
 
 public class SpeedUp extends Item {
 
+    /**
+     * This value decides how much the character's speed is incremented by.
+     */
     int speedIncrement = 1;
 
+    /**
+     * Activates the item by setting its point worth and duration. It increments the speed of the character by the value of speedIncrement.
+     * @param grid Is used to increment the current score.
+     * @param pac Used to access the step variable in the pacman character.
+     * @param timeLimit Not used in this function, but it is a parameter required by the Item class that SpeedUp extends.
+     */
     @Override
     void activate(GridReadCreate grid, Pacman pac, int[] timeLimit)
     {
@@ -14,6 +23,11 @@ public class SpeedUp extends Item {
         pac.step += speedIncrement;
     }
 
+    /**
+     * Deactivates the item by restoring any values it has originally modified.
+     * @param grid Not used in this function, but it is a parameter required by the Item class that SpeedUp extends.
+     * @param pac Used to access the step variable to restore it to the default quantity.
+     */
     @Override
     public void deactivate(GridReadCreate grid, Pacman pac)
     {
